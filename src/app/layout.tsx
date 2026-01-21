56e308e260e84ca1ea0d854fbe83b85292435216
Rasil07 <rasilgrt@gmail.com>
2026-01-21 12:35:43 +1100

    testing lints

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
+const unusedvar = 42;
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -24,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <p>{unusedvar}</p>
+        {children}
+      </body>
     </html>
   );
 }
